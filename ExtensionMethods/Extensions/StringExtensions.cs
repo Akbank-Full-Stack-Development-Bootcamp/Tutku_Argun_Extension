2d6e66529be9e0c1449a5719d861a8901625d60a
Tutku Argun <tutkuargun@outlook.com>
2021-06-05 23:31:46 +0300

    Proje dosyası ekle.

---
+++ ExtensionMethods/Extensions/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethods.Extensions
+{
+    public static class StringExtensions
+    {
+       public static string GetReverse(this string name)
+        {
+            
+            string result = string.Empty;
+            for (int i = name.Length-1; i >= 0; i--)
+            {
+                result += name[i];
+            }
+            
+            return result;
+        }
+
+        
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,20 @@
+﻿using ExtensionMethods.Extensions;
+using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen isminizi yazın: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("İşte isminizin tersten yazılışı : ");
+            Console.WriteLine(name.GetReverse());
+            
+            
+            
+        }
+    }
+}
